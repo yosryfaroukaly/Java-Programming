a38c84f0f3e6a63628c508564cf7bbd1458eaeb0
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-02 18:41:37 -0600

    Day 2

---
+++ src/day03_comments_escape_sequence/Goals.java
@@ -0,0 +1,13 @@
+package day03_comments_escape_sequence;
+
+public class Goals {
+
+    public static void main(String[] args) {
+
+        System.out.println("Make everyone love java");
+        System.out.println("have a good time");
+        System.out.println("Be better than you were yesterday");
+
+    }
+
+}
