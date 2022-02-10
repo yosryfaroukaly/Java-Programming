87fc33288c0e2310bd43843f22bf34cce25e8b26
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-09 20:27:36 -0600

    Day 8

--- src/day08_relational_operators/AgeGroup.java
+++ src/day08_relational_operators/AgeGroup.java
@@ -13,4 +13,17 @@ public class AgeGroup {
 
      */
 
+    public static void main(String[] args) {
+
+        int age = 13;
+
+        boolean isKid = age <= 13;
+        boolean isSenior = age >= 65;
+
+        System.out.println("You are a kid: " + isKid);
+        System.out.println("You are a senior citizen: " + isSenior);
+
+
+    }
+
 }
