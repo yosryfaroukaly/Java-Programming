93183dacd395e4200caff540eb0cfb1b38cd329d
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-09 20:15:50 -0600

    Day 8

---
+++ src/day08_relational_operators/AgeGroup.java
@@ -0,0 +1,16 @@
+package day08_relational_operators;
+
+public class AgeGroup {
+
+    /*
+    task:
+
+        declare and assign an age variable
+
+        is the person a kid ( up to 13, include 13)
+
+        is the person a senior citizen ( 65 and above)
+
+     */
+
+}
