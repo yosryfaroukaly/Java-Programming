7c44f54ea1646d8953ebe8abbcb39eed8a99ff28
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-12 15:21:40 -0600

    Day 9

---
+++ src/day09_scanner/AddNumbers.java
@@ -0,0 +1,9 @@
+package day09_scanner;
+
+public class AddNumbers {
+    /*
+        create Scanner object
+        ask the user to enter 3 numbers
+        find and print the sum of the numbers
+     */
+}
