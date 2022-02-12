880ba31ee75b1d8c65ea44b2bd269e66e83dfd1a
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-12 09:32:23 -0600

    Day 9

---
+++ src/day09_scanner/OperatorPractice.java
@@ -0,0 +1,16 @@
+package day09_scanner;
+
+public class OperatorPractice {
+    public static void main(String[] args) {
+        int w = 78 / 2 * 2 + 3 - 5 % 5;
+        /*        39 * 2 + 3 - 5 % 5
+                  78 + 3 - 5 % 5
+                  78 + 3 - 0
+                  78 + 3
+                  81
+         */
+        System.out.println(w);
+
+
+    }
+}
