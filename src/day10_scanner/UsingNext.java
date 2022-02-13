3407e40f4bb2bf9ba9ec8cd94a69343e12e538e9
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-13 11:45:21 -0600

    Day 10

--- src/day10_scanner/UsingNext.java
+++ src/day10_scanner/UsingNext.java
@@ -14,11 +14,17 @@ public class UsingNext {
         String firstName = input.next();
 
         System.out.println("Enter your last name");
-        String lastName = input.next();
+        String lastName = input.next(); // to send this data, you need to hit enter
+
+        input.nextLine(); // the enter input from the line above is absorbed here
+
+        System.out.println("What is your address");
+        String address = input.nextLine();
 
         System.out.println("Today: "+ day);
         System.out.println("First name " + firstName);
         System.out.println("Last name " + lastName);
+        System.out.println("Address " + address);
 
 
     }
