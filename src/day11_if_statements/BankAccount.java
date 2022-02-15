f8500ba9a8f9dafb05aef95a176f94b7383bb26e
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-14 21:07:55 -0600

    Day 11 if statements

--- src/day11_if_statements/BankAccount.java
+++ src/day11_if_statements/BankAccount.java
@@ -31,7 +31,7 @@ public class BankAccount {
         // withdraw some amount of money from my balance
         balance -= withdraw; // balance = balance - withdraw
 
-        if(balance < 0){
+        if (balance < 0) {
             System.out.println("Took out too much money, $100 overdraft applied");
             System.out.println("Balance before fee: " + balance);
             balance -= 100; // balance = balance - 100;
