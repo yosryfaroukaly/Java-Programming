1ba337c60a01419aee7f1bd4e38711114e2ac7b1
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-19 12:00:59 -0600

    Day 14 nested if

--- src/day14_if_statements/LoginApp.java
+++ src/day14_if_statements/LoginApp.java
@@ -55,6 +55,41 @@ outputs:
         }
 
 
+
+        /*
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your pincode: ");
+        int actualPin = input.nextInt();
+        System.out.println("Please enter the last 4 digits of your ssn");
+        int actualSSN = input.nextInt();
+
+        int expectedPin = 1552;
+        int expectedSSN = 1234;
+        boolean validPin = actualPin == expectedPin;
+        boolean validSSN = actualSSN == expectedSSN;
+
+        if( validPin && validSSN ){
+            System.out.println("Authentication successful");
+        } else {
+
+            System.out.println("Authentication failed");
+
+            if(!validPin){
+                System.out.println("incorrect pin code");
+            }
+
+            if(!validSSN){
+                System.out.println("incorrect ssn");
+            }
+
+        }
+         */
+        // valid pin: true -> !true -> false
+        // ssn wrong: false -> !false -> true
+
+
     }
 
 }
