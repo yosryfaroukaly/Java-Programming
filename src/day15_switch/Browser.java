2dddce4a3248966ecfd086f8ea4e51187c4108a5
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-02-20 11:55:13 -0600

    Day 15 switch

---
+++ src/day15_switch/Browser.java
@@ -0,0 +1,32 @@
+package day15_switch;
+
+import java.util.Scanner;
+
+public class Browser {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your browser");
+        String browser = input.nextLine();
+        System.out.println("Enter your url");
+        String url = input.nextLine();
+
+        switch (browser){
+            case "chrome":
+            case "Chrome":
+                System.out.println("Opening " + url + " in the chrome browser");
+                System.out.println("Loading...");
+                break;
+            case "Safari":
+                System.out.println("Opening " + url + " in safari");
+                break;
+            case "Firefox":
+                System.out.println("Opening " + url + " in firefox");
+                break;
+            default:
+                System.out.println(browser + " is not a valid browser");
+        }
+
+
+    }
+}
