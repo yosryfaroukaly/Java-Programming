7f0178e7a6784ed0303b19dc3fd3efc4de29af66
Mehmet <mhmtsytrk@gmail.com>
2022-02-22 18:37:56 -0500

    Officehours P03 is  DONE

---
+++ officeHours/day03_02_22_2022/P04_Discount.java
@@ -0,0 +1,22 @@
+package day03_02_22_2022;
+
+public class P04_Discount {
+    public static void main(String[] args) {
+        /*
+        			Task : 		A book store will give discount of 10% if the cost of purchased quantity is more than 1000.
+							Create a quantity variable
+							    Suppose, one unit will cost 100.
+							    Judge and print total cost for user.
+					            ( Scanner Class can be used )
+
+							INPUT for quantity :  5 		 							EXPECTED : No discount applied
+
+							INPUT for quantity  :  15									EXPECTED : You get a discount of $totalDiscount and your total cost is $totalCost 																						   your total cost is $totalCost
+
+         */
+
+
+
+
+    }
+}
