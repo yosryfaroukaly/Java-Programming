3614704f3acf54231773095223afeee265c7cdca
Mehmet <mhmtsytrk@gmail.com>
2022-02-22 18:49:51 -0500

    Officehours P04 is  DONE

--- officeHours/day03_02_22_2022/P04_Discount.java
+++ officeHours/day03_02_22_2022/P04_Discount.java
@@ -9,11 +9,34 @@ public class P04_Discount {
 							    Judge and print total cost for user.
 					            ( Scanner Class can be used )
 
-							INPUT for quantity :  5 		 							EXPECTED : No discount applied
+							INPUT for quantity :  5 		 	EXPECTED : No discount applied
 
-							INPUT for quantity  :  15									EXPECTED : You get a discount of $totalDiscount and your total cost is $totalCost 																						   your total cost is $totalCost
+							INPUT for quantity  :  15			EXPECTED : You get a discount of $totalDiscount and your total cost is $totalCost 																						   your total cost is $totalCost
 
          */
+
+
+        int quantity=15;
+        int unitPrice=100;
+        double totalCost=quantity*unitPrice;     // 1500
+        double discountRate=0.10;
+        double totalDiscount=0;
+
+
+        if(totalCost>=1000){
+
+            // I will apply discount
+            totalDiscount=discountRate*totalCost;   // 150
+            System.out.println("You get a discount of "+totalDiscount+" and your total cost is "+(totalCost-totalDiscount));
+
+        }
+        else
+        {
+
+            // will not apply any discount
+            System.out.println("No discount applied");
+        }
+
 
 
 
