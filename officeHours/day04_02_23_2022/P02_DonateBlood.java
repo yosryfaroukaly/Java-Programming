fa62708941e21050b6dd0759c924b91862229204
Mehmet <mhmtsytrk@gmail.com>
2022-02-23 18:00:07 -0500

    Officehours P02 is  DONE

--- officeHours/day04_02_23_2022/P02_DonateBlood.java
+++ officeHours/day04_02_23_2022/P02_DonateBlood.java
@@ -1,10 +1,11 @@
 package day04_02_23_2022;
 
+import java.util.Scanner;
+
 public class P02_DonateBlood {
     public static void main(String[] args) {
-        /*    ( Scanner Class can be used )
-
-
+        /*
+              ( Scanner Class can be used )
 					        Create a program that accepts age and weight(kg) as int.
 
 					        If age greater than or equal 18 and weight greater than 50 kg give a message
@@ -17,5 +18,33 @@ public class P02_DonateBlood {
 					        	EXPECTED  :  "Age must be greater than 18"
 
       				   */
+
+        Scanner sc=new Scanner(System.in);
+        int age = sc.nextInt();
+        int weight=82;
+
+
+        if(age>=18){
+            // check another condition.
+            if (weight>=50){
+                System.out.println("You are eligible to donate blood");
+            }else {
+                System.out.println("You are not eligible to donate blood");
+            }
+        }else{
+            System.out.println("Age must be greater than 18");
+
+        }
+
+
+
+
+
+
+
+
+
+
+
     }
 }
