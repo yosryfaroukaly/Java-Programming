6d83e6e6295965826d5d8d04890fc66936ff42bb
Mehmet <mhmtsytrk@gmail.com>
2022-02-23 17:49:35 -0500

    Officehours P02 is  started

---
+++ officeHours/day04_02_23_2022/P02_DonateBlood.java
@@ -0,0 +1,21 @@
+package day04_02_23_2022;
+
+public class P02_DonateBlood {
+    public static void main(String[] args) {
+        /*    ( Scanner Class can be used )
+
+
+					        Create a program that accepts age and weight(kg) as int.
+
+					        If age greater than or equal 18 and weight greater than 50 kg give a message
+					        	EXPECTED  :  "You are eligible to donate blood"
+
+					        If age greater than or equal 18 and weight smaller or equal than 50 kg give a message
+					        	EXPECTED  :  "You are not eligible to donate blood"
+
+					        If age is smaller than 18 "Age must be greater than 18"
+					        	EXPECTED  :  "Age must be greater than 18"
+
+      				   */
+    }
+}
