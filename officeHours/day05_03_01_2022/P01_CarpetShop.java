0574c6f5e145570e3e9885ed9ea375fa45f61b7a
Mehmet <mhmtsytrk@gmail.com>
2022-03-01 18:08:24 -0500

    Officehours P01 is started

---
+++ officeHours/day05_03_01_2022/P01_CarpetShop.java
@@ -0,0 +1,33 @@
+package day05_03_01_2022;
+
+public class P01_CarpetShop {
+    public static void main(String[] args) {
+
+        /*
+         - Ask the user which room do they need carpet? (String)
+                            			List of Rooms
+                            				Bedroom
+                            				Kitchen
+
+                            -> If the user gives other than these roomTypes ,  give an error message about "Invalid Room Type"
+
+                            		- Ask the user roomType dimensions (as meter and double)
+                            			Kitchen is circle.Take radius from user
+                            			Bedroom is square or Rectangle.Take lenght and width from user
+
+                            			- Ask the user to delivery address (String, multiple words)
+                            			- unitPriceForCarpet = 3.92
+                            			- totalcost = area*unitPriceForCarpet;
+
+                            - Print in the following format:
+                            		Ex:
+                            			inputs: Bedroom,length 4,width 3,1300 Lamar St Houston TX 77010
+
+                            			"TotalCost for your carpet is 47.04 and it will be delivery 1300 Lamar St Houston TX 77010 in one week"
+                                     */
+
+
+
+
+    }
+}
