802c7d813899ec96b27072dfbd2e2b557f5e8691
Mehmet <mhmtsytrk@gmail.com>
2022-03-02 18:30:51 -0500

    Officehours P02 is started

---
+++ officeHours/day06_03_02_2022/P02_SearchResult.java
@@ -0,0 +1,22 @@
+package day06_03_02_2022;
+
+public class P02_SearchResult {
+    public static void main(String[] args) {
+        /*
+        Task 2 : Assume that you searched a word in Google  Search Engine.After that you took a result message like this
+
+
+	   "About 3,640,000,000 results (0.78 seconds)"
+
+		According to this result print out search Time and number of Result
+
+		Input :  "About 640,000,000 results (0.78 seconds)"
+
+		Output:
+					Result number is 640,000,000
+					Response time is 0.78 seconds
+         */
+
+
+    }
+}
