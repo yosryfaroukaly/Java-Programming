0cf88d6af0b4c3ef27fcec4cec89174eaf201558
Mehmet <mhmtsytrk@gmail.com>
2022-03-02 17:58:59 -0500

    Officehours P01 is started

---
+++ officeHours/day06_03_02_2022/P01_CategoryName.java
@@ -0,0 +1,26 @@
+package day06_03_02_2022;
+
+public class P01_CategoryName {
+    /*
+    Task 1 : Assume that you searched a baby toy in Etsy.When you open related category your url is :
+
+		"https://www.etsy.com/c/toys-and-games/toys/baby-and-toddler-toys?ref=catnav-11049"
+			    		       (main category)
+
+		According to this result print out main category name
+
+			Input :  https://www.etsy.com/c/toys-and-games/toys/baby-and-toddler-toys?ref=catnav-11049
+
+			Output:
+				Main Category name is  toys and games
+
+			Input :  https://www.etsy.com/c/toys-and-entertainment?ref=catnav-11049
+						 				     (main category)
+
+			Output:
+				Main Category name is  toys and entertainment
+     */
+    public static void main(String[] args) {
+
+    }
+}
