a1b93a528147ef01c704949d8eb3f80aabfaa518
Mehmet <mhmtsytrk@gmail.com>
2022-03-02 18:30:03 -0500

    Officehours P01 is DONE

--- officeHours/day06_03_02_2022/P01_CategoryName.java
+++ officeHours/day06_03_02_2022/P01_CategoryName.java
@@ -22,5 +22,28 @@ public class P01_CategoryName {
      */
     public static void main(String[] args) {
 
+
+        String url ="https://www.etsy.com/c/toys-and-games/toys/baby-and-toddler-toys?ref=catnav-11049";
+
+        int startIndex=url.indexOf("c/");
+        int endIndex=url.indexOf("?");
+
+        String categoryName=url.substring(startIndex+2,endIndex);
+        System.out.println(categoryName);
+
+        categoryName=categoryName.replace('-',' ');
+
+        if(categoryName.contains("/")){
+            categoryName=categoryName.substring(0,categoryName.indexOf("/"));
+        }
+
+        System.out.println(categoryName);
+
+
+
+
+
+
+
     }
 }
