f7ebb21948ee634c12208007b3b82712f6973784
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-06 14:31:58 -0600

    Day 24 loops practice

--- src/day24_loops/BranchingExample.java
+++ src/day24_loops/BranchingExample.java
@@ -21,6 +21,26 @@ public class BranchingExample {
             }
         }
 
+        System.out.println();
+        for(int i = 0; i <= 10; i++){
+            if(i == 5){
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+        for(int i = 1; i <= 100; i++){
+
+            if(i % 2 == 1){
+                continue;
+            }
+
+            System.out.print(i + " ");
+
+        }
+
+
 
     }
 }
