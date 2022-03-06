b6c3789f59fe6f78c1c2683d12e4035f966e2e84
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-06 12:01:04 -0600

    Day 24 loops practice

---
+++ src/day24_loops/CountChars.java
@@ -0,0 +1,43 @@
+package day24_loops;
+
+public class CountChars {
+    /*
+    Count upper, lower, and numbers
+        Given a String, find and print how many uppercase letter, lowercase letter, and number are in the String.
+        Ex:
+        Input: 2juMp41EEkd4s4
+
+        Output:
+        3 uppercase letters
+        6 lowercase letters
+        5 numbers
+     */
+
+    public static void main(String[] args) {
+
+        String str = "2juMp41EEkd4s4";
+        int upper = 0;
+        int lower = 0;
+        int number = 0;
+
+        for(int i = 0; i < str.length(); i++){
+
+            char letter = str.charAt(i); // get the character at the index number i for this iteration, and stores into the variable letter for re-usability
+
+            if(letter >= 'A' && letter <= 'Z'){
+                upper++;
+            } else if (letter >= 'a' && letter <= 'z') {
+                lower++;
+            } else if(letter >= '0' && letter <= '9'){
+                number++;
+            }
+
+        }
+
+        System.out.println("Uppercase characters: " + upper);
+        System.out.println("Lowercase characters: " + lower);
+        System.out.println("Number characters: " + number);
+
+
+    }
+}
