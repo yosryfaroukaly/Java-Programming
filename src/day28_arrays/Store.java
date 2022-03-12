6615f2b3d5123563f6fbed970e20173ac0cc3967
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-12 15:54:01 -0600

    Day 28 Arrays

--- src/day28_arrays/Store.java
+++ src/day28_arrays/Store.java
@@ -1,6 +1,7 @@
 package day28_arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Store {
 
@@ -37,6 +38,20 @@ public class Store {
 
         // use Scanner to ask which item they are looking for, and check if we have it
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Which item are you looking for");
+        String lookingFor = input.nextLine();
+        boolean inStock = false;
 
+        for(int i = 0; i < items.length; i++){
+
+            if(items[i].equalsIgnoreCase(lookingFor)){
+                inStock = true;
+                break;
+            }
+
+        }
+
+        System.out.println(lookingFor + (inStock ? " in stock" : " out of stock"));
     }
 }
