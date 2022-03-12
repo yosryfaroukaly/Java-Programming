af93a8c4ae40100551d99fd8380979506bc339dc
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-12 10:37:07 -0600

    Day 28 Arrays

--- src/day28_arrays/ArrayIntro.java
+++ src/day28_arrays/ArrayIntro.java
@@ -1,5 +1,7 @@
 package day28_arrays;
 
+import java.util.Arrays;
+
 public class ArrayIntro {
     public static void main(String[] args) {
 
@@ -17,8 +19,6 @@ public class ArrayIntro {
 
         String [] cities2 = {cityOne, cityTwo, cityThree, cityFour, cityFive};
 
-        System.out.println(cities); // this doesn't print the array how we want
-
         // how to access each element in the array
 
         System.out.println(cities[0]);
@@ -26,6 +26,17 @@ public class ArrayIntro {
         System.out.println(cities[2]);
         System.out.println(cities[3]);
         System.out.println(cities[4]);
+
+        // print all the cities in one line
+
+        System.out.println("Reverse order of the cities: " + cities[4] + ", " + cities[3] + ", " + cities[2] + ", " + cities[1] + ", " + cities[0]);
+
+        // print the array all at one time
+        System.out.println(cities); // this doesn't print the array how we want
+        System.out.println(Arrays.toString(cities)); // this method from Arrays class lets you print the array fully
+
+        // number of elements
+        System.out.println(cities.length);
 
 
     }
