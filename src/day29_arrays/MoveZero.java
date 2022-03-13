18e53e3272be54288d1477e5c7953ee032809648
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-13 13:40:47 -0500

    Day 29 Arrays

--- src/day29_arrays/MoveZero.java
+++ src/day29_arrays/MoveZero.java
@@ -15,14 +15,21 @@ public class MoveZero {
      */
     public static void main(String[] args) {
 
-        int [] nums = {10, 0, 5, 0, 0, 0, 0, 1, 0};
+        int [] nums = {10, 0, 0, 0, 0, 5, 0, 1, 0};
         int [] fixed = new int[nums.length];
         int index = 0; // variable to keep track of indexes of the fixed array
 
-        for(int i = 0; i < nums.length; i++){
+//        for(int i = 0; i < nums.length; i++){
+//
+//            if(nums[i] != 0){
+//                fixed[index++] = nums[i]; //reading the element at position i of the nums array and storing it into the fixed array and position index. after storing the value index variable is incremented by 1
+//            }
+//        }
 
-            if(nums[i] != 0){
-                fixed[index++] = nums[i]; //reading the element at position i of the nums array and storing it into the fixed array and position index. after storing the value index variable is incremented by 1
+        for(int each : nums){
+            if(each != 0){
+                fixed[index] = each;
+                index++;
             }
         }
 
