32b2800f261a246f3b51942fb46a65094051e86b
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-13 14:07:17 -0500

    Day 29 Arrays

--- src/day29_arrays/Initials.java
+++ src/day29_arrays/Initials.java
@@ -12,9 +12,9 @@ public class Initials {
 
     public static void main(String[] args) {
 
-        String [] names = {"James Bond", "Eve Rell", "Anna Johnson", "       Willy Orange"};
+        String[] names = {"James Bond", "Eve Rell", "Anna Johnson", "       Willy Orange"};
 
-        for(int i = 0; i < names.length; i++){
+        for (int i = 0; i < names.length; i++) {
 
             String fullName = names[i].trim();
             String lastName = fullName.substring(fullName.indexOf(" ")).trim(); // indexOf(" ") + 1
@@ -22,15 +22,16 @@ public class Initials {
 
         }
 
-        for(String str : names){
+        for (String str : names) {
 
             String fullName = str.trim();
             String lastName = fullName.substring(fullName.indexOf(" ")).trim(); // indexOf(" ") + 1
             System.out.println("" + fullName.charAt(0) + lastName.charAt(0));
 
-          //  System.out.println(str.trim().substring(0,1) + str.substring(str.indexOf(" ")).trim().charAt(0));
+            // option 2:
+//            str = str.trim();
+//            System.out.println(str.substring(0, 1) + str.substring(str.indexOf(" ")).trim().charAt(0));
         }
-
 
 
     }
