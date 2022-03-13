e5131167d1973a3b426798094fa5961e8844250a
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-13 15:42:21 -0500

    Day 29 Arrays

--- src/day29_arrays/Store.java
+++ src/day29_arrays/Store.java
@@ -2,13 +2,13 @@ package day29_arrays;
 
 public class Store {
     public static void main(String[] args) {
-        String [] items = { "Shoes", "Jackets", "Gloves", "AirPod", "Ipod", "Backpack"};
-        double [] prices = { 89.99, 150.0, 999.99, 250.0, 439.5, 39.99 };
-        int [] itemIds = { 12345, 12346, 12347, 12348, 12349, 12350 };
+        String[] items = {"Shoes", "Jackets", "Gloves", "AirPod", "Ipod", "Backpack"};
+        double[] prices = {89.99, 150.0, 399.99, 250.0, 439.5, 39.99};
+        int[] itemIds = {12345, 12346, 12347, 12348, 12349, 12350};
 
         // print the catalog
 
-        for(int i = 0; i < items.length; i++){
+        for (int i = 0; i < items.length; i++) {
 
             System.out.println("Item ID: " + itemIds[i] + " are " + items[i] + " and they cost $" + prices[i]);
 
@@ -20,8 +20,8 @@ public class Store {
 
         int indexOfGloves = -1; // we set -1 as the default, so that if gloves is not found in the array it will always give -1
 
-        for(int i = 0; i < items.length; i++){
-            if(items[i].equalsIgnoreCase("gloves")){
+        for (int i = 0; i < items.length; i++) {
+            if (items[i].equalsIgnoreCase("gloves")) {
                 indexOfGloves = i;
                 break;
             }
@@ -29,7 +29,23 @@ public class Store {
 
         System.out.println("Index of gloves " + indexOfGloves);
 
+        // Task: Find and print the information of the most expensive item. Name, price, id
+        System.out.println();
 
+        double mostExpensive = prices[0];
+        int indexOfMostExpensive = 0;
+
+        for(int i = 0; i < prices.length; i++){
+            if(prices[i] > mostExpensive){
+                mostExpensive = prices[i];
+                indexOfMostExpensive = i;
+            }
+        }
+
+        System.out.println("The most expensive item: ");
+        System.out.println("Item: " + items[indexOfMostExpensive]);
+        System.out.println("Item ID: " + itemIds[indexOfMostExpensive]);
+        System.out.println("Price $" + prices[indexOfMostExpensive]);
 
     }
 }
