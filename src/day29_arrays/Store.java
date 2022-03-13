451be1b718ffae81a1ee66cf120311e8ef9fdeb4
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-13 14:38:57 -0500

    Day 29 Arrays

---
+++ src/day29_arrays/Store.java
@@ -0,0 +1,9 @@
+package day29_arrays;
+
+public class Store {
+    public static void main(String[] args) {
+        String [] items = { "Shoes", "Jackets", "Gloves", "AirPod", "Ipod", "Backpack"};
+        double [] prices = { 89.99, 150.0, 999.99, 250.0, 439.5, 39.99 };
+        int [] itemIds = { 12345, 12346, 12347, 12348, 12349, 12350 };
+    }
+}
