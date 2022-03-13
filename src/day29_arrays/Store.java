c96fa341d86551a9ab50e552f0e004bb6af23fed
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-13 14:47:39 -0500

    Day 29 Arrays

--- src/day29_arrays/Store.java
+++ src/day29_arrays/Store.java
@@ -5,5 +5,22 @@ public class Store {
         String [] items = { "Shoes", "Jackets", "Gloves", "AirPod", "Ipod", "Backpack"};
         double [] prices = { 89.99, 150.0, 999.99, 250.0, 439.5, 39.99 };
         int [] itemIds = { 12345, 12346, 12347, 12348, 12349, 12350 };
+
+        // print the catalog
+
+        for(int i = 0; i < items.length; i++){
+
+            System.out.println("Item ID: " + itemIds[i] + " are " + items[i] + " and they cost $" + prices[i]);
+
+        }
+
+        System.out.println();
+
+        // Task: Find the index of Gloves
+
+
+
+
+
     }
 }
