6b04db6797fd2bac01b2d93340ed06253ec4ba3a
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-14 20:29:12 -0500

    Day 30 Arrays

---
+++ src/day30_arrays/ReverseSentence.java
@@ -0,0 +1,13 @@
+package day30_arrays;
+
+public class ReverseSentence {
+    /*
+
+        Given a sentence
+            today is monday
+
+         reverse the sentence
+            monday is today
+
+     */
+}
