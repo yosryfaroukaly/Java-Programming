3e0e948194fb913e2f28dcff2186624f7f61144d
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-19 10:05:25 -0500

    Day 32 Arrays

--- src/day32_arrays/Recap.java
+++ src/day32_arrays/Recap.java
@@ -27,11 +27,10 @@ public class Recap {
 
         String str = "Today is Saturday";
 
-        System.out.println(Arrays.toString(str.toCharArray()));
+        System.out.println(str.split(" "));
+        System.out.println(Arrays.toString(str.toCharArray())); // returns char [] to print as a String
 
-        System.out.println(Arrays.toString(str.split(" ")));
-
-
+        System.out.println(Arrays.toString(str.split(" "))); // returns String [] to print as a String
 
     }
 }
