30a0617acb4bf9b83dc14ba338fb29095795e9ce
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-19 10:25:14 -0500

    Day 32 Arrays

---
+++ src/day32_arrays/AddElement.java
@@ -0,0 +1,12 @@
+package day32_arrays;
+
+public class AddElement {
+
+    /*
+
+    Have a starting array
+    you want to add a number to the end
+    so make your array bigger to store the new number
+
+     */
+}
