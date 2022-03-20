9a5cf45c7af7929af34d80003ce6f33fe1a6ff0c
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-20 10:48:55 -0500

    Day 33 Methods

---
+++ src/day33_methods/Car.java
@@ -0,0 +1,47 @@
+package day33_methods;
+
+public class Car {
+
+    /*
+
+    unlock the car
+    open the door
+    sit in the car
+    close the door
+    put on seatbelt
+    start the engine
+    put into drive and go
+
+     */
+
+    public static void unlockCar(){
+        System.out.println("Unlocking the car");
+    }
+
+    public static void openDoor(){
+        System.out.println("Opening the door");
+    }
+
+    public static void sitInCar(){
+        System.out.println("Sitting in the car");
+        System.out.println("Closing the door");
+    }
+
+    public static void getReadyToGo(){
+        System.out.println("Putting on seatbelt");
+        System.out.println("Check mirrors");
+        System.out.println("Put on music");
+        System.out.println("Put on navigation");
+    }
+
+    public static void startCar(){
+        System.out.println("Starting car");
+    }
+
+    public static void driveAndGo(){
+        System.out.println("Putting car into drive");
+        System.out.println("Going forwards");
+    }
+
+
+}
