dfc784ce661a508daffd0a9662d511c7b3523d87
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-20 11:38:35 -0500

    Day 33 Methods

---
+++ src/day33_methods/UseGetCharacters.java
@@ -0,0 +1,12 @@
+package day33_methods;
+
+public class UseGetCharacters {
+    public static void main(String[] args) {
+
+        GetCharacters.printAtoZUpper();
+        GetCharacters.printAtoZLower();
+        GetCharacters.print0To9();
+
+
+    }
+}
