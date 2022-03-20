e34174f2c0bc131da4c8b2213e469b7145e31cc7
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-20 14:44:22 -0500

    Day 33 Methods

---
+++ src/day33_methods/OurArrayClass.java
@@ -0,0 +1,20 @@
+package day33_methods;
+
+public class OurArrayClass {
+
+    /*
+
+        all will accept array argument
+
+        create a method that will print the first element
+        create a method that will print the last element
+        create a method that will print the element/s
+            length matters -> even there is middle, for odd there is one middle
+
+     */
+
+    public static void firstElement(int [] nums){
+        System.out.println("First Element: " + nums[0]);
+    }
+
+}
