49b66bcfe5e8c985bde40e4564891245f3b811a3
Mehmet <mhmtsytrk@gmail.com>
2022-03-22 18:22:50 -0400

    OfficeHours Day 11

---
+++ officeHours/day11_03_22_2022/P04_StringTimes.java
@@ -0,0 +1,14 @@
+package day11_03_22_2022;
+
+public class P04_StringTimes {
+    public static void main(String[] args) {
+        /*
+          Task 1 :    Write a method that accepts a string and a non-negative number,
+            	and prints a larger string that is number copies of the original String
+
+                stringTimes("Hi", 2); = > HiHi
+                stringTimes("Hi", 3); = > HiHiHi
+                stringTimes("Hi", 1); = > Hi
+         */
+    }
+}
