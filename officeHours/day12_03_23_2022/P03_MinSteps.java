66c6e2a47c68e4ef24153603e3d60491d96e31b6
Mehmet <mhmtsytrk@gmail.com>
2022-03-23 18:21:10 -0400

    OfficeHours Day 12

---
+++ officeHours/day12_03_23_2022/P03_MinSteps.java
@@ -0,0 +1,23 @@
+package day12_03_23_2022;
+
+public class P03_MinSteps {
+    public static void main(String[] args) {
+        /*
+         Task 5 :     Number of Steps to Main directory  --- Interview Task
+
+    			 We have a record of every time a change of directory is performed.The different change directory functions are as follow ;
+
+    				 ../  Move to parent folder of current folder
+    				 ./   Remain in the same folder
+    				 x/   Move to the child folder named x
+
+
+    				 Write a method to Find the minimum steps required to go back to main directory from current folder.The squence of operations performed after entering the main directory provided
+
+
+    				 Input : {"x/","y/","../","z/","./"}
+
+    				 Output : 2
+         */
+    }
+}
