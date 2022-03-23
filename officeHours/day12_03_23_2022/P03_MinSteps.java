4e071d9b51f005cd4713052ee8f3f2e79400ea0c
Mehmet <mhmtsytrk@gmail.com>
2022-03-23 18:38:22 -0400

    OfficeHours Day 12

--- officeHours/day12_03_23_2022/P03_MinSteps.java
+++ officeHours/day12_03_23_2022/P03_MinSteps.java
@@ -19,5 +19,33 @@ public class P03_MinSteps {
 
     				 Output : 2
          */
+
+        String[] input={"x/","y/","../","z/","./","JavaProgramingB26/","../"};
+        minSteps(input);
     }
+
+    public static void minSteps(String[] steps){
+
+        int count=0;
+
+
+        for (String eachStep : steps) {
+
+            switch (eachStep){
+
+                case "./":
+                    break;
+                case "../":
+                    count--;
+                    break;
+                default:
+                    count++;
+                    break;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+
 }
