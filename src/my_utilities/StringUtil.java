0dd3c8cea44136043704737cbea21d596eee684c
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-23 20:58:19 -0500

    Day 34 Methods

---
+++ src/my_utilities/StringUtil.java
@@ -0,0 +1,19 @@
+package my_utilities;
+
+public class StringUtil {
+
+    /*
+        reverse
+        create a method that will accept a String and return the reversed version
+     */
+
+    public static String reverse(String str){
+        String reversed = "";
+        for(int i = str.length() - 1; i >= 0; i--){
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
+
+
+}
