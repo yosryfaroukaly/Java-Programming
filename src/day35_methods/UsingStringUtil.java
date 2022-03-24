2bf6a2c0cff705547b61773534ad733faa324e9e
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-24 18:53:42 -0500

    Day 34 Methods

---
+++ src/day35_methods/UsingStringUtil.java
@@ -0,0 +1,15 @@
+package day35_methods;
+
+import my_utilities.StringUtil;
+
+public class UsingStringUtil {
+    public static void main(String[] args) {
+
+        System.out.println(StringUtil.reverse("apple"));
+
+        System.out.println(StringUtil.fixFormat("jaMES"));
+
+
+
+    }
+}
