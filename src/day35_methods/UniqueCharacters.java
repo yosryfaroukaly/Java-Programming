e9080a88d5cb9ad71adc877f95fa0550b2e3b099
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-24 19:53:30 -0500

    Day 34 Methods

--- src/day35_methods/UniqueCharacters.java
+++ src/day35_methods/UniqueCharacters.java
@@ -21,13 +21,39 @@ Ex:
     public static String uniqueCharacters(String str){
 
         String unique = "";
+        String checked = "";
 
         for(int i = 0; i < str.length(); i++){
 
+            if(checked.contains("" + str.charAt(i))){
+                continue;
+            }
+
             int count = StringUtil.frequencyOfCharacter(str, str.charAt(i));
+            checked += str.charAt(i);
 
             if(count == 1){
                 unique += str.charAt(i);
+            }
+        }
+
+        return unique;
+    }
+
+    public static String uniqueCharacters2(String str){
+
+        String unique = "";
+        String checked = "";
+
+        for(int i = 0; i < str.length(); i++){
+
+            if(!checked.contains("" + str.charAt(i))){
+                int count = StringUtil.frequencyOfCharacter(str, str.charAt(i));
+                checked += str.charAt(i);
+
+                if(count == 1){
+                    unique += str.charAt(i);
+                }
             }
         }
 
--- src/my_utilities/StringUtil.java
+++ src/my_utilities/StringUtil.java
@@ -48,4 +48,32 @@ public class StringUtil {
         return count;
     }
 
+        /*
+        Unique Characters
+
+        create a method that will accept a word(String) and return a String with all the unique characters. A character is unique if it only appears in the String once.
+
+        Hint: use your frequency method to do some of the work
+     */
+
+    public static String uniqueCharacters(String str){
+
+        String unique = "";
+        String checked = "";
+
+        for(int i = 0; i < str.length(); i++){
+
+            if(!checked.contains("" + str.charAt(i))){
+                int count = StringUtil.frequencyOfCharacter(str, str.charAt(i));
+                checked += str.charAt(i);
+
+                if(count == 1){
+                    unique += str.charAt(i);
+                }
+            }
+        }
+
+        return unique;
+    }
+
 }
