a0c8176716d2d81db453d8dad58678b4e8f26858
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-24 19:43:17 -0500

    Day 34 Methods

---
+++ src/day35_methods/UniqueCharacters.java
@@ -0,0 +1,52 @@
+package day35_methods;
+
+import my_utilities.StringUtil;
+
+public class UniqueCharacters {
+    /*
+    Unique Characters
+
+create a method that will accept a word(String) and return a String with all the unique characters. A character is unique if it only appears in the String once.
+
+Hint: use your frequency method to do some of the work
+
+Ex:
+	Input:
+		aaaaabcccdeeff
+
+	Output:
+		bd
+     */
+
+    public static String uniqueCharacters(String str){
+
+        String unique = "";
+
+        for(int i = 0; i < str.length(); i++){
+
+            int count = StringUtil.frequencyOfCharacter(str, str.charAt(i));
+
+            if(count == 1){
+                unique += str.charAt(i);
+            }
+        }
+
+        return unique;
+    }
+}
+
+
+/*
+		aaaaabcccdeeff
+ int count = StringUtil.frequencyOfCharacter(str, str.charAt(i));
+
+    int i = 0
+    StringUtil.frequencyOfCharacter(str, str.charAt(0));
+    StringUtil.frequencyOfCharacter(aaaaabcccdeeff, a);
+    --> 5
+    ...
+     i = 5
+    StringUtil.frequencyOfCharacter(str, str.charAt(5));
+    StringUtil.frequencyOfCharacter(aaaaabcccdeeff, b);
+     -> 1
+ */
