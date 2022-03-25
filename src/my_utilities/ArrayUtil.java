77b014f25d52b209717da5d77adfd5fc40dc72cf
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-24 20:49:50 -0500

    Day 34 Methods

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -38,4 +38,23 @@ public class ArrayUtil {
         return max;
     }
 
+            /*
+        Contains
+
+    create a method that will accept an int array and an int number. Check and return if the given number is in the array.
+         */
+
+    public static boolean contains(int[] nums, int element) {
+
+        for (int each : nums) {
+
+            if (each == element) {
+                return true;
+            }
+
+        }
+
+        return false;
+    }
+
 }
