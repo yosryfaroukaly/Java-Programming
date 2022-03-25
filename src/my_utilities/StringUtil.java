1352d5baba84f8c06433b89793049f26e9c38874
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-24 19:21:36 -0500

    Day 34 Methods

--- src/my_utilities/StringUtil.java
+++ src/my_utilities/StringUtil.java
@@ -7,25 +7,45 @@ public class StringUtil {
         create a method that will accept a String and return the reversed version
      */
 
-    public static String reverse(String str){
+    public static String reverse(String str) {
         String reversed = "";
-        for(int i = str.length() - 1; i >= 0; i--){
+        for (int i = str.length() - 1; i >= 0; i--) {
             reversed += str.charAt(i);
         }
         return reversed;
     }
 
         /*
-    Fix Format
+        Fix Format
 
-    create a method will accept a String and return a String in proper format. Proper format is:
-    First character starting as uppercase and the rest as lowercase
+        create a method will accept a String and return a String in proper format. Proper format is:
+        First character starting as uppercase and the rest as lowercase
 
-     */
+         */
 
-    public static String fixFormat(String str){
+    public static String fixFormat(String str) {
         str = str.trim();
-        return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase();
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+       /*
+        Frequency of Character
+
+        create a method that will accept a word(String) and a letter (char) and return how many times that given letter is found in the given word
+
+        */
+
+    public static int frequencyOfCharacter(String str, char letter) {
+
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == letter) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
 }
