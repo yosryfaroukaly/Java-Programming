a8e56d1aa2e24b55102d629a538c18baabd682ad
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-24 20:27:36 -0500

    Day 34 Methods

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -1,4 +1,41 @@
 package my_utilities;
 
 public class ArrayUtil {
+        /*
+    Min Number
+
+    create a method that will accept an int array and return the smallest number from the array
+     */
+
+    public static int minNumber(int [] nums){
+
+        int min = nums[0];
+
+        for(int num : nums){
+            if(num < min){
+                min = num;
+            }
+        }
+        return min;
+    }
+
+    /*
+    Max Number
+
+    create a method that will accept an int array and return the biggest number from the array
+
+     */
+
+    public static int maxNumber(int [] nums){
+
+        int max = nums[0];
+
+        for(int num : nums){
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
+    }
+
 }
