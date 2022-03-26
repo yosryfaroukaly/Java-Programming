00c2c19531ff36fdc7caa47454c3bfe0e327dfbb
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-26 09:33:19 -0500

    Day 36 Methods

---
+++ src/day36_methods/Picture.java
@@ -0,0 +1,26 @@
+package day36_methods;
+
+public class Picture {
+
+    public static void draw(){
+        System.out.println("trying to draw");
+    }
+
+    public static void draw(String color){
+        System.out.println("draw with " + color);
+    }
+
+    public static void draw(String color, String color2){
+        System.out.println("drawing with color " + color + " and with   " + color2);
+    }
+
+    public static void draw(int size){
+        System.out.println("drawing with size " + size);
+    }
+
+    // not valid by just changing the parameter name, but if you added two int parameters then it would work
+//    public static void draw(int length){
+//        System.out.println("drawing with size " + length);
+//    }
+
+}
