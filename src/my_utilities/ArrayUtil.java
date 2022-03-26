9ff10684f878eb7e71db1faaa64396413954b46a
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-26 10:36:35 -0500

    Day 36 Methods

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -57,4 +57,27 @@ public class ArrayUtil {
         return false;
     }
 
+
+    public static int indexOf(String[] strs, String element) {
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].equals(element)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+
+    public static int indexOf(int[] nums, int element) {
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == element) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
 }
