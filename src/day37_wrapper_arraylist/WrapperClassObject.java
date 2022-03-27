1b4f71847dd865aded8d93570a0c9ba709d14e39
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-27 10:57:45 -0500

    Day 37 Wrapper class and ArrayList

--- src/day37_wrapper_arraylist/WrapperClassObject.java
+++ src/day37_wrapper_arraylist/WrapperClassObject.java
@@ -7,12 +7,12 @@ public class WrapperClassObject {
         // What can we do with this: assign/reassign, math, casting
         // What can't we do with this: a.method()
 
-        Integer a2 = new Integer(10);
+        //Integer a2 = new Integer(10);
         Integer a3 = 20; // autoboxing - primitive -> object
         int a4 = a3; // unboxing - object -> primitive
 
         System.out.println(a);
-        System.out.println(a2);
+       // System.out.println(a2);
         System.out.println(a3);
 
         byte b = 2;
