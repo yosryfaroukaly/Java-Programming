9436cf435536107385dc3205216b09adf10ff272
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-27 09:56:02 -0500

    Day 37 Wrapper class and ArrayList

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -80,4 +80,35 @@ public class ArrayUtil {
         return -1;
     }
 
+    public static int [] addElement(int [] original, int elementToAdd){
+        int [] newArray = new int[original.length + 1];
+
+        for(int i = 0; i < original.length; i++){
+            newArray[i] = original[i];
+        }
+        // Instead of doing a loop ourselves, we could have used Arrays.copyOf()
+        newArray[newArray.length - 1] = elementToAdd; // adding the new element to the end of the new array
+        return newArray;
+    }
+
+
+    public static int [] addElement(int [] original, int [] elementsToAdd){
+
+        int [] newArray = new int[original.length + elementsToAdd.length];
+
+        for(int i = 0, j = 0; i < newArray.length; i++){
+
+            if(i < original.length){
+                // added the elements from the original array in the new array
+                newArray[i] = original[i];
+            } else {
+                // added the new elements to the new array
+                newArray[i] = elementsToAdd[j++];
+            }
+
+        }
+
+        return newArray;
+    }
+
 }
