e35533ac95a8d6f0b35130f21d3b3d98df667320
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-28 18:50:03 -0500

    Day 37 Wrapper class and ArrayList

---
+++ src/day38_arraylist/HtmlGenerator.java
@@ -0,0 +1,49 @@
+package day38_arraylist;
+
+import java.util.Arrays;
+
+public class HtmlGenerator {
+
+    /*
+    HTML Generator
+
+Given a String in the following format take the number part of the generator the html tags.
+	Tags are surrounded with diamond brackets. The number of times to repeat with be given as the number after the ^
+
+	Ex:
+		Input:
+			div^2
+		Output:
+			<div></div> <div></div>
+
+	Ex:
+		Input:
+			li^3
+		Output:
+			<li></li><li></li><li></li>
+     */
+
+    public static void main(String[] args) {
+        System.out.println(htmlGenerate("li^3"));
+        System.out.println(htmlGenerate("div^6"));
+        System.out.println(htmlGenerate("ul^4"));
+    }
+
+    public static String htmlGenerate(String str){
+
+        String html = "";
+        String [] parts = str.split("\\^"); // str.replace("^" , " ").split(" ");
+
+        int repeat = Integer.parseInt(parts[1]);
+
+        for(int i = 0; i < repeat; i++){
+
+            html += "<" + parts[0] + "></" + parts[0] + "> ";
+
+        }
+
+        return html;
+    }
+
+
+}
