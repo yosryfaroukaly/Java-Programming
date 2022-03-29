97f3d824369ed6d3b31182ae05e1ef05a5888c1f
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-28 20:57:02 -0500

    Day 38 ArrayList

--- src/day38_arraylist/RemoveExample.java
+++ src/day38_arraylist/RemoveExample.java
@@ -16,12 +16,24 @@ public class RemoveExample {
         list.remove(0); // returns: water, but we didn't use it
         System.out.println(list); // [sun, wind, wood]
 
-        System.out.println(list.remove(list.size() - 1));
-        //list.size() - 1 --> last index
+        System.out.println(list.remove(2));
+        // list.size() - 1 --> last index
         // remove(last index) --> remove element: wood
         // prints the removed element: wood
 
         System.out.println(list); // [sun, wind]
+
+        list.remove("sun");
+        System.out.println(list);
+
+        System.out.println(list.remove("apple")); // return false because I don't have an element of apple
+        System.out.println(list);
+
+        list.add("light");
+        list.add("light");
+        System.out.println(list);
+        list.remove("light");
+        System.out.println(list);
 
     }
 }
