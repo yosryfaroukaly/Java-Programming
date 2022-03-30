f2181576252c1eb61b3aa077003aa20725ace1de
Mehmet <mhmtsytrk@gmail.com>
2022-03-30 18:31:55 -0400

    OfficeHours Day 14

---
+++ officeHours/day14_03_30_2022/P01_SumUpToZero.java
@@ -0,0 +1,14 @@
+package day14_03_30_2022;
+
+public class P01_SumUpToZero {
+    public static void main(String[] args) {
+        /*
+           Task 3:  Write a method that,
+                       given an integer N (1 < N < 100), returns an array containing N unique integers that sum up to 0.
+                        The method can return any such array.
+                        For example, given N = 4, the function could return [1,0, -3,2] or [-2,1, -4,5].
+                        The answer [1, -1,1,3] would be incorrect (because value 1 occurs twice).
+                        For N = 3 one of the possible answers is [-1,0,1] (but there are many more correct answers).
+         */
+    }
+}
