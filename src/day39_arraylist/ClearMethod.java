65f782f2c989805e943339e66a74e7e5aa918538
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-03-30 19:26:05 -0500

    Day 39 ArrayList

---
+++ src/day39_arraylist/ClearMethod.java
@@ -0,0 +1,15 @@
+package day39_arraylist;
+
+import java.util.ArrayList;
+
+public class ClearMethod {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(100);
+        nums.add(200);
+        nums.add(500);
+        nums.add(800);
+
+    }
+}
