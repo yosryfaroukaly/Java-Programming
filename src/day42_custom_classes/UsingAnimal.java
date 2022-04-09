148d12cf842c53750bddb30e93a83bed184c8da5
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-09 14:20:50 -0500

    Day 42 Custom classes

---
+++ src/day42_custom_classes/UsingAnimal.java
@@ -0,0 +1,15 @@
+package day42_custom_classes;
+
+public class UsingAnimal {
+    public static void main(String[] args) {
+
+        Animal bird = new Animal();
+        bird.population = 10000000;
+        bird.species = "Bird";
+
+        System.out.println(bird);
+        System.out.println(bird.toString()); // no how we will use it
+
+
+    }
+}
