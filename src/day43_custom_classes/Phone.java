081670569e1b1ae7a73d71379a4e3661732d2ff0
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-10 14:48:13 -0500

    Day 43 Custom classes

---
+++ src/day43_custom_classes/Phone.java
@@ -0,0 +1,52 @@
+package day43_custom_classes;
+
+public class Phone {
+
+    /*
+
+        create instance variables
+
+            name, brand, memory, version
+
+        create constructors
+
+        - accept and initialize the name
+
+        - accept and initialize the name and brand
+
+        - accept and initialize the name, brand, memory, and version
+
+        create a toString
+     */
+
+    String name;
+    String brand;
+    int memory;
+    double version;
+
+    public Phone(String name) {
+        this.name = name;
+    }
+
+    public Phone(String name, String brand) {
+        this.name = name;
+        this.brand = brand;
+    }
+
+    public Phone(String name, String brand, int memory, double version) {
+        this.name = name;
+        this.brand = brand;
+        this.memory = memory;
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "name='" + name + '\'' +
+                ", brand='" + brand + '\'' +
+                ", memory=" + memory +
+                ", version=" + version +
+                '}';
+    }
+}
