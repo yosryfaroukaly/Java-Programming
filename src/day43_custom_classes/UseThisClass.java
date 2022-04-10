62b0f350b14f6e2cc10f0d76556a6cab690349ab
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-10 13:54:00 -0500

    Day 43 Custom classes

---
+++ src/day43_custom_classes/UseThisClass.java
@@ -0,0 +1,10 @@
+package day43_custom_classes;
+
+public class UseThisClass {
+    public static void main(String[] args) {
+
+        ThisKeyword obj = new ThisKeyword(300);
+        System.out.println(obj.a);
+
+    }
+}
