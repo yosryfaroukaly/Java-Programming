273eceb91696ad09e66c43bde57b04e4f2f6cdb2
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-11 20:03:16 -0500

    Day 44 Custom classes

--- src/day44_custom_classes/Song.java
+++ src/day44_custom_classes/Song.java
@@ -12,20 +12,23 @@ public class Song {
     }
 
     public Song(String name, double length){
-        this.name = name;
+//        this.name = name;
+        this(name);
         this.length = length;
     }
 
     public Song(String name, double length, String artist) {
-        this.name = name;
-        this.length = length;
+//        this.name = name;
+//        this.length = length;
+        this(name, length);
         this.artist = artist;
     }
 
     public Song(String name, double length, String artist, String genre) {
-        this.name = name;
-        this.length = length;
-        this.artist = artist;
+//        this.name = name;
+//        this.length = length;
+//        this.artist = artist;
+        this(name, length, artist);
         this.genre = genre;
     }
 }
