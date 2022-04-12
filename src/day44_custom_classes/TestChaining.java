6b65f792e7f07cfec16ac5a1a13bc7e10d05c20c
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-11 20:55:43 -0500

    Day 44 Custom classes

---
+++ src/day44_custom_classes/TestChaining.java
@@ -0,0 +1,14 @@
+package day44_custom_classes;
+
+public class TestChaining {
+    public static void main(String[] args) {
+
+        new ChainExample(10);
+
+        System.out.println();
+
+        new ChainExample("hello");
+
+
+    }
+}
