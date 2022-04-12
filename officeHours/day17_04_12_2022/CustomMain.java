fcb877ed4e4d29fd227ab96b91160539e7539a2f
Mehmet <mhmtsytrk@gmail.com>
2022-04-12 18:35:38 -0400

    OfficeHours Day 17

---
+++ officeHours/day17_04_12_2022/CustomMain.java
@@ -0,0 +1,38 @@
+package day17_04_12_2022;
+
+public class CustomMain {
+
+    public static void main(String[] args) {
+
+
+        // Create a new OBJECT from CustomClassIntro
+
+
+        new CustomClassIntro();
+        // there is no reference to
+
+        System.out.println("==============");
+        CustomClassIntro c=new CustomClassIntro();
+        System.out.println(c);
+
+        System.out.println("==============");
+        // Call instance Var
+        c.instanceVariable="Instance Variable from Main Method";
+        System.out.println(c);
+
+
+        System.out.println("==============");
+        // Call instance method
+        c.instanceMethod();
+        System.out.println(c);
+
+
+        // Create a new OBJECT
+
+        System.out.println("==============");
+        CustomClassIntro c2=new CustomClassIntro();
+        System.out.println(c2.toString());
+
+
+    }
+}
