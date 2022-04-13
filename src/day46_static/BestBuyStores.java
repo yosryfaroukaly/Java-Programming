6005890690172b520935509a448909c23b108b30
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-13 18:50:05 -0500

    Day 46 static

---
+++ src/day46_static/BestBuyStores.java
@@ -0,0 +1,32 @@
+package day46_static;
+
+public class BestBuyStores {
+    public static void main(String[] args) {
+
+        // access the static members by the class name
+        System.out.println(BestBuy.headquarters);
+        System.out.println(BestBuy.day);
+        System.out.println(BestBuy.numberOfComputer);
+
+        BestBuy store1 = new BestBuy("Oak Drive");
+        System.out.println(store1.location); // accessing the instance variable
+        System.out.println(store1.numberOfComputer);
+
+        store1.numberOfComputer--;
+
+        System.out.println(BestBuy.numberOfComputer);
+
+        BestBuy store2 = new BestBuy("Main St");
+        System.out.println(store2.location);
+
+        store2.numberOfComputer -= 5;
+
+        System.out.println(store1.numberOfComputer);
+        System.out.println(BestBuy.numberOfComputer);
+
+        store2.location = "Fairfax";
+        System.out.println(store1.location);
+        System.out.println(store2.location);
+
+    }
+}
