e0c2235a5c021b34eecb11f73f7bfb86acf3c719
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-13 20:33:04 -0500

    Day 46 static

--- src/day46_static/IPhone.java
+++ src/day46_static/IPhone.java
@@ -7,11 +7,19 @@ public class IPhone {
 
     static String company;
     static String os;
+    static boolean appleDay;
+    static String day;
 
     static {
         System.out.println("STATIC BLOCK RUN");
         company = "Apple";
         os = "IOS";
+        day = "Wednesday";
+
+        if(day.equals("Friday")){
+            appleDay = true;
+        }
+
     }
 
     public IPhone(String model, double price) {
