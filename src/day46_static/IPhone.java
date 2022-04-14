559e63586c3c1b3d69714dea5ad7a8cf69a2ba36
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-13 20:10:07 -0500

    Day 46 static

---
+++ src/day46_static/IPhone.java
@@ -0,0 +1,30 @@
+package day46_static;
+
+public class IPhone {
+
+    String model;
+    double price;
+
+    static String company;
+    static String os;
+
+    static {
+        System.out.println("STATIC BLOCK RUN");
+        company = "Apple";
+        os = "IOS";
+    }
+
+    public IPhone(String model, double price) {
+        System.out.println("CONSTRUCTOR RUN");
+        this.model = model;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "IPhone{" +
+                "model='" + model + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
