ad778dcffdc8761a9fa67a3e1c7c016ce2605676
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-16 10:50:55 -0500

    Day 47 static

---
+++ src/day47_encapsulation/Computer.java
@@ -0,0 +1,38 @@
+package day47_encapsulation;
+
+public class Computer {
+    /*
+    Computer - static
+
+    class name: computer:
+        instance variables:
+            price, brand, color, ...
+
+        static variables: hasScreen, hasBattery, hasMemory
+
+        make objects of the class in a main method and access both instance and static members from proper references
+
+     */
+
+    String brand;
+    double price;
+    String color;
+
+    static boolean hasScreen;
+    static boolean hasBattery;
+    static boolean hasMemory;
+
+    static {
+        System.out.println("Static block run");
+        hasScreen = false;
+        hasBattery = true;
+        hasMemory = true;
+    }
+
+    public Computer(String brand, double price, String color) {
+        System.out.println("Constructor run");
+        this.brand = brand;
+        this.price = price;
+        this.color = color;
+    }
+}
