78bfc1ce97e6a9e70351ee3bef65cbf567701219
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-16 15:56:21 -0500

    Day 47 Encapsulation

---
+++ src/day47_encapsulation/TrafficLight.java
@@ -0,0 +1,23 @@
+package day47_encapsulation;
+
+public class TrafficLight {
+
+    private String color;
+
+    public TrafficLight(String color){
+        setColor(color);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        switch (color.toLowerCase()){
+            case "red":
+            case "yellow":
+            case "green":
+                this.color = color;
+        }
+    }
+}
