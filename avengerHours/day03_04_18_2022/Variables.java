e330a806c9a2de771683abb9c75b1693893fcbbe
austinu <austin@cydeo.com>
2022-04-18 18:49:59 -0400

    avenger hour day3

---
+++ avengerHours/day03_04_18_2022/Variables.java
@@ -0,0 +1,12 @@
+package day03_04_18_2022;
+
+public class Variables {
+    double price; //primitive - instance variable (belong to obj)
+
+    static Integer count ; //non primitive - static (belongs Variables class)
+
+
+    public void method1(){
+      Integer num = 100; //non primitive - local (belong to method1() method)
+    }
+}
