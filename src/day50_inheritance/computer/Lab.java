0cd68bffb7e109dc1bba05641215a19581290a5c
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-20 20:41:29 -0500

    Day 50 Inheritance

---
+++ src/day50_inheritance/computer/Lab.java
@@ -0,0 +1,29 @@
+package day50_inheritance.computer;
+
+import java.awt.*;
+
+public class Lab {
+    public static void main(String[] args) {
+
+        Computer obj1 = new Computer("generic os", 300);
+        System.out.println(obj1);
+
+        Windows obj2 = new Windows(500);
+        //             new Computer("Windows", 500)
+        System.out.println(obj2);
+
+        Mac obj3 = new Mac(300);
+        System.out.println(obj3);
+
+    }
+}
+
+/*
+version when you accept both os and memory
+Mac obj3 = new Mac("Mac", 300);
+new Mac("Mac", 300);
+new Mac("Mac", 200);
+new Mac("Mac", 1000);
+new Mac("Mac", 400);
+
+ */
---
+++ src/day50_inheritance/computer/Windows.java
@@ -0,0 +1,13 @@
+package day50_inheritance.computer;
+
+public class Windows extends Computer{
+
+    public Windows(int memory){
+        super("Windows", memory);
+    }
+
+    // calls the parent constructor, which sets the os variable to be "Windows" and sets the memory variable to be the argument from the Windows constructor
+
+}
+
+// Windows is a Computer
