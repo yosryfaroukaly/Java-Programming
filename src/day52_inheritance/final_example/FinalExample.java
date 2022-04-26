5cb086d4bfa6d3f3ede768d1bb7690788569679a
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-26 18:56:30 -0500

    Day 52 Inheritance

---
+++ src/day52_inheritance/final_example/FinalExample.java
@@ -0,0 +1,14 @@
+package day52_inheritance.final_example;
+
+public final class FinalExample {
+
+    final int a = 5;
+    final int b;
+
+    public static final String PLANET = "Earth";  // constant variable
+
+    public FinalExample(int b){
+        this.b = b;
+    }
+
+}
