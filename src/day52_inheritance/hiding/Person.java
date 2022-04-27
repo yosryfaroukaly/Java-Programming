f345242eeb320bf70c97c870c524e7b3e6ad6144
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-26 21:02:28 -0500

    Day 52 Inheritance

---
+++ src/day52_inheritance/hiding/Person.java
@@ -0,0 +1,22 @@
+package day52_inheritance.hiding;
+
+public class Person {
+
+    String name = "James Bond";
+
+}
+
+class SecretIdentity extends Person{
+
+    String name = "DNOB SEMAJ"; // we created another name variable in the sub class, but it was already being inherited, so now we are hidding the one from the parent
+}
+
+class Runner {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.name);
+
+        SecretIdentity secretIdentity = new SecretIdentity();
+        System.out.println(secretIdentity.name);
+    }
+}
