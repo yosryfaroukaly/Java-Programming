2ee2e86dfa52ff451d97eb2e5ceede093ba26603
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-26 20:28:14 -0500

    Day 52 Inheritance

---
+++ src/day52_inheritance/hiding/Soccer.java
@@ -0,0 +1,21 @@
+package day52_inheritance.hiding;
+
+public class Soccer extends Sports{
+
+
+    public static void cheer(){
+        System.out.println("Cheering from the Child class");
+    }
+
+    /*
+        Note:
+
+        it looks like we are overriding
+        but we are not, because the method static
+
+        we are hiding the cheer method from the parent class
+
+
+     */
+
+}
---
+++ src/day52_inheritance/hiding/Sports.java
@@ -0,0 +1,9 @@
+package day52_inheritance.hiding;
+
+public class Sports {
+
+    public static void cheer(){
+        System.out.println("Cheering from the Parent class");
+    }
+
+}
