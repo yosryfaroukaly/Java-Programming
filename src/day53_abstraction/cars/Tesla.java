2fc5a327fe98fc9e9b1d5453620fb5d4a2e0b4cc
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-27 20:24:29 -0500

    Day 52 Abstraction

---
+++ src/day53_abstraction/cars/Tesla.java
@@ -0,0 +1,27 @@
+package day53_abstraction.cars;
+
+public class Tesla extends ElectricCar{
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void charge() {
+
+    }
+}
+
+/*
+
+    the Tesla class is a sub classes of the ElectricCar class
+
+    the ElectricCar class is abstract and the Tesla is not abstract (non-abstract)
+
+    normal class(non-abstract) cannot have abstract methods
+
+    so, we have to override any/all abstract method that were inherited
+
+
+ */
