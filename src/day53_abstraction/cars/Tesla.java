d541dd896a0b71c37de125d9d47d1c95f8aa61d0
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-04-27 20:33:43 -0500

    Day 52 Abstraction

--- src/day53_abstraction/cars/Tesla.java
+++ src/day53_abstraction/cars/Tesla.java
@@ -4,12 +4,13 @@ public class Tesla extends ElectricCar{
 
     @Override
     public void start() {
-
+        System.out.println("card in the car");
+        System.out.println("press button");
     }
 
     @Override
     public void charge() {
-
+        System.out.println("charging tesla");
     }
 }
 
