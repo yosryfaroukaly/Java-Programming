3baf009653f2caf8c648b39996e800ec90d16fe3
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-05-03 20:10:02 -0500

    Day 53 Abstraction

---
+++ src/day54_abstraction/language/Language.java
@@ -0,0 +1,9 @@
+package day54_abstraction.language;
+
+public interface Language {
+
+    void hi();
+
+    void bye();
+
+}
---
+++ src/day54_abstraction/language/Translator.java
@@ -0,0 +1,19 @@
+package day54_abstraction.language;
+
+public class Translator {
+    public static void main(String[] args) {
+
+        //Language obj = new Language(); cannot create objects of interface
+
+        Spanish spanish = new Spanish();
+        spanish.hi();
+        spanish.bye();
+
+        Turkish turkish = new Turkish();
+        turkish.hi();
+        turkish.bye();
+
+
+
+    }
+}
