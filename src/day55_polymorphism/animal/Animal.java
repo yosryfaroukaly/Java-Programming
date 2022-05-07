4e5ec7fc4a3ebc770000d559b9a344196d9db777
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-05-07 14:14:59 -0500

    Day 55 Polymorphism

---
+++ src/day55_polymorphism/animal/Animal.java
@@ -0,0 +1,9 @@
+package day55_polymorphism.animal;
+
+public class Animal {
+
+    public void eat(){
+        System.out.println("ANIMAl is eating");
+    }
+
+}
---
+++ src/day55_polymorphism/animal/Lizard.java
@@ -0,0 +1,10 @@
+package day55_polymorphism.animal;
+
+public class Lizard extends Reptile{
+    @Override
+    public void eat() {
+        System.out.println("LIZARD is eating");
+    }
+}
+
+
