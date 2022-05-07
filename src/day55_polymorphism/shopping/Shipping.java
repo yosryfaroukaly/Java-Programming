6f983454c02e643642f6dee2cf8257bbcd918a5f
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-05-07 09:52:19 -0500

    Day 55 Abstraction

---
+++ src/day55_polymorphism/shopping/Shipping.java
@@ -0,0 +1,12 @@
+package day55_polymorphism.shopping;
+
+/*
+Create an interface Shipping
+    - Declare an abstract methods:
+        payForShipping(boolean)
+ */
+public interface Shipping {
+
+    void payForShipping(boolean b);
+
+}
