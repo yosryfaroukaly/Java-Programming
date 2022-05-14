0fcdc23f57d112a7faf779110f6e62ac1ef3b2b8
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-05-14 12:08:33 -0500

    day57_exceptions

---
+++ src/day57_exceptions/learn/FirstTry.java
@@ -0,0 +1,22 @@
+package day57_exceptions.learn;
+
+import java.util.jar.JarOutputStream;
+
+public class FirstTry {
+    public static void main(String[] args) {
+
+        System.out.println("First Line");
+
+        try{
+
+            String word = "java";
+
+            System.out.println(word.charAt(0));
+
+        } catch (Exception e){
+            System.out.println("Exception happened");
+        }
+
+        System.out.println("Last Line");
+    }
+}
