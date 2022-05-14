3bd2353c123c19110b0e909901be2f9323b333ac
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-05-14 14:43:24 -0500

    day57_exceptions

--- src/day57_exceptions/learn/ThirdTry.java
+++ src/day57_exceptions/learn/ThirdTry.java
@@ -12,14 +12,21 @@ public class ThirdTry {
             System.out.println("enter a number");
             int num = input.nextInt();
 
+            System.out.println("enter number 2");
+            int num2 = input.nextInt();
+
+            System.out.println(num/num2);
 
         }catch (InputMismatchException obj){
             obj.printStackTrace(); // shows the exception message in the console
+        }catch (ArithmeticException e){
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("FINALLY BLOCK");
+            input.close();
         }
 
-
         System.out.println("DONE");
-
 
 
     }
