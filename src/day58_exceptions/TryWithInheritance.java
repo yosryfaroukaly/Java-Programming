dce9847fdaac7b7a2cf41fbc108be060f7e7bc79
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-05-15 15:23:01 -0500

    day58_exceptions

--- src/day58_exceptions/TryWithInheritance.java
+++ src/day58_exceptions/TryWithInheritance.java
@@ -1,10 +1,11 @@
 package day58_exceptions;
 
+import java.util.Collection;
+
 public class TryWithInheritance {
     public static void main(String[] args) {
 
         try{
-
 
 
         } catch (IndexOutOfBoundsException e){
