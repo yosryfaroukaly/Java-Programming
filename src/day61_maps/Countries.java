23e22f1a363d8b860eb65c373793ecd454463a1c
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-05-21 15:35:55 -0500

    Maps

---
+++ src/day61_maps/Countries.java
@@ -0,0 +1,42 @@
+package day61_maps;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Countries {
+    public static void main(String[] args) {
+
+        Map<String, ArrayList<String>> countries = new HashMap<>();
+        // created a HashMap that has key of String type - countries and ArrayList<String> for the value and those will be cities
+
+        countries.put("United States", new ArrayList<>(Arrays.asList(
+                "Chicago", "New York", "Dallas", "DC", "Denver", "LA"
+        )));
+        countries.put("Turkey", new ArrayList<>(Arrays.asList(
+                "Istanbul", "Ankara", "Izmir"
+        )));
+        countries.put("Canada", new ArrayList<>(Arrays.asList(
+                "Quebec", "Toronto", "Vancouver"
+        )));
+
+        System.out.println(countries);
+
+        // Print all cities that start with D or I
+
+        for (ArrayList<String> cities : countries.values()) { // iterates through the values of the map, which are the ArrayList of cities
+
+            for (String eachCity : cities) { // iterates through each city in each ArrayList value
+
+                if (eachCity.startsWith("D") || eachCity.startsWith("I")) {
+                    System.out.println(eachCity);
+                }
+
+            }
+
+        }
+
+
+    }
+}
