fd89bb66386c4c9a9860cb63c4fb4a025984a3bb
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-04 10:41:56 -0500

    day 65 - Bye world

--- src/day65_streams/Recap.java
+++ src/day65_streams/Recap.java
@@ -2,7 +2,10 @@ package day65_streams;
 
 import day54_abstraction.creating.Create;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 
 public class Recap {
@@ -61,6 +64,35 @@ public class Recap {
             return true;
 
         };
+
+        int [] arr3 = {1,2,3,4,8,7};
+        int [] arr4 = {7,8,10};
+        System.out.println(firstContains.test(arr3, arr4));
+
+        System.out.println();
+
+
+        // merge two arrays into a list
+        // first two generics are for the parameters
+        // last generic is for the return type
+
+        // 1, 2, 3
+        // 4, 5, 6
+        BiFunction<int[], int [], List<Integer>> mergeToList = (array1, array2) -> {
+
+            List<Integer> list = new ArrayList<>();
+
+            for(int i = 0 ; i < array1.length; i++){
+                list.add(array1[i]);
+                list.add(array2[i]);
+            }
+            return list;
+        };
+
+        int [] arr5 = {1, 2, 3};
+        int [] arr6 = {4, 5, 6};
+        System.out.println(mergeToList.apply(arr5, arr6));
+
 
 
 
