deb61d2cec92790380f4d018370d6fa70f55d3b8
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-04 15:27:54 -0500

    day 65 - Bye world

---
+++ src/day65_streams/Item.java
@@ -0,0 +1,46 @@
+package day65_streams;
+
+public class Item {
+    private String name;
+    private double price;
+    private int stars;
+
+    public Item(String name, double price, int stars) {
+        this.name = name;
+        this.price = price;
+        this.stars = stars;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", stars=" + stars +
+                '}';
+    }
+}
