695daf7d2d420360d5353cd80ed9be056bbe9bd3
Cydeo-Saim <57418084+saimCybertek@users.noreply.github.com>
2022-06-04 10:29:57 -0500

    day 65 - Bye world

--- src/day65_streams/Recap.java
+++ src/day65_streams/Recap.java
@@ -1,5 +1,7 @@
 package day65_streams;
 
+import day54_abstraction.creating.Create;
+
 import java.util.Arrays;
 import java.util.function.BiPredicate;
 
@@ -27,6 +29,40 @@ public class Recap {
         int [] arr2 = {2, 1, 3};
         System.out.println(arrayIsEqual.test(arr1, arr2));
         System.out.println(Arrays.toString(arr1));
+
+        System.out.println();
+
+        /*
+       1.2 Create a function that can check if the first array contains all the elements of the second array:
+        ex: arr1 = {1,2,3,4,5,7}
+        arr2 = {7,8}
+
+        output: false
+         */
+
+        BiPredicate<int [], int []> firstContains = (array1, array2) -> {
+
+            for(int outer : array2){ // 1
+
+                boolean check = false;
+
+                for(int inner : array1){
+                    if(outer == inner){
+                        check = true;
+                        break;
+                    }
+                }
+
+                if(!check) {
+                    return false; // because the number was not in the first array
+                }
+            }
+
+            return true;
+
+        };
+
+
 
 
     }
